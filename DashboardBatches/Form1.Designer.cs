a1b68d9c0e4e5574139a2894239a8cd68792a01c
Wind <deachawat9937@gmail.com>
2025-02-11 10:27:13 +0700

    First Prototype PILOT DB - Version 0.1.0

---
+++ DashboardBatches/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace MyProjectNamespace
+{
+    partial class DashboardBatches
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Panel headerPanel;
+        private System.Windows.Forms.PictureBox logoPictureBox;
+        private System.Windows.Forms.ComboBox comboBoxLine;
+        private System.Windows.Forms.DataGridView dataGridViewBatches;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.headerPanel = new System.Windows.Forms.Panel();
+            this.logoPictureBox = new System.Windows.Forms.PictureBox();
+            this.comboBoxLine = new System.Windows.Forms.ComboBox();
+            this.dataGridViewBatches = new System.Windows.Forms.DataGridView();
+            this.headerPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBatches)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // headerPanel
+            // 
+            this.headerPanel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.headerPanel.Controls.Add(this.logoPictureBox);
+            this.headerPanel.Controls.Add(this.comboBoxLine);
+            this.headerPanel.Dock = System.Windows.Forms.DockStyle.Top;
+            this.headerPanel.Location = new System.Drawing.Point(0, 0);
+            this.headerPanel.Name = "headerPanel";
+            this.headerPanel.Size = new System.Drawing.Size(1500, 137);
+            this.headerPanel.TabIndex = 1;
+            // 
+            // logoPictureBox
+            // 
+            this.logoPictureBox.Location = new System.Drawing.Point(20, 10);
+            this.logoPictureBox.Name = "logoPictureBox";
+            this.logoPictureBox.Size = new System.Drawing.Size(229, 108);
+            this.logoPictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.logoPictureBox.TabIndex = 0;
+            this.logoPictureBox.TabStop = false;
+            // 
+            // comboBoxLine
+            // 
+            this.comboBoxLine.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxLine.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.comboBoxLine.Location = new System.Drawing.Point(300, 25);
+            this.comboBoxLine.Name = "comboBoxLine";
+            this.comboBoxLine.Size = new System.Drawing.Size(250, 40);
+            this.comboBoxLine.TabIndex = 1;
+            this.comboBoxLine.SelectedIndexChanged += new System.EventHandler(this.ComboBoxLine_SelectedIndexChanged);
+            // 
+            // dataGridViewBatches
+            // 
+            this.dataGridViewBatches.AllowUserToAddRows = false;
+            this.dataGridViewBatches.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridViewBatches.ColumnHeadersHeight = 50;
+            this.dataGridViewBatches.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridViewBatches.Location = new System.Drawing.Point(0, 137);
+            this.dataGridViewBatches.Name = "dataGridViewBatches";
+            this.dataGridViewBatches.ReadOnly = true;
+            this.dataGridViewBatches.RowHeadersWidth = 62;
+            this.dataGridViewBatches.RowTemplate.Height = 50;
+            this.dataGridViewBatches.Size = new System.Drawing.Size(1500, 663);
+            this.dataGridViewBatches.TabIndex = 0;
+            this.dataGridViewBatches.CellFormatting += new System.Windows.Forms.DataGridViewCellFormattingEventHandler(this.DataGridViewBatches_CellFormatting);
+            // 
+            // DashboardBatches
+            // 
+            this.ClientSize = new System.Drawing.Size(1500, 800);
+            this.Controls.Add(this.dataGridViewBatches);
+            this.Controls.Add(this.headerPanel);
+            this.Name = "DashboardBatches";
+            this.Load += new System.EventHandler(this.DashboardBatches_Load);
+            this.headerPanel.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.logoPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewBatches)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+    }
+}
---
+++ DashboardBatches/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+
+namespace MyProjectNamespace
+{
+    public partial class DashboardBatches : Form
+    {
+        private string connectionString =
+                 @"Data Source=th-bp-db\mssql2017;" +
+                 "User ID=dvl;" +
+                 "Password=Pr0gr@mm1ng;" +
+                 "Database=TFCPILOT;" +
+                 "Application Name=DashboardBatches";
+
+        public DashboardBatches()
+        {
+            InitializeComponent();
+        }
+
+        private void DashboardBatches_Load(object sender, EventArgs e)
+        {
+            LoadCompanyLogo();
+            LoadLineMachines();
+
+            if (comboBoxLine.Items.Count > 0)
+            {
+                comboBoxLine.SelectedIndex = 0;
+            }
+
+            LoadBatchData();
+        }
+
+        private void ComboBoxLine_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            LoadBatchData();
+        }
+
+        private void LoadCompanyLogo()
+        {
+            string imageUrl = "https://img2.pic.in.th/pic/logo1.jpg";
+
+            try
+            {
+                using (WebClient webClient = new WebClient())
+                {
+                    byte[] imageBytes = webClient.DownloadData(imageUrl);
+                    using (MemoryStream ms = new MemoryStream(imageBytes))
+                    {
+                        logoPictureBox.Image = Image.FromStream(ms);
+                    }
+                }
+            }
+            catch
+            {
+                MessageBox.Show("โหลดโลโก้ไม่สำเร็จ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LoadLineMachines()
+        {
+            comboBoxLine.Items.Clear();
+            comboBoxLine.Items.Add("Aussie");
+            comboBoxLine.Items.Add("Yankee");
+            comboBoxLine.Items.Add("Seasoning");
+        }
+
+        private void LoadBatchData()
+        {
+            string selectedLine = comboBoxLine.SelectedItem?.ToString() ?? "Aussie";
+
+            string sql = @"
+SELECT 
+    BatchNo, StartSieve, FinishSieve, RMSieveBinID, SieveComplete, RMTipBinID, StartTip, 
+    FinishTip, StartBlend, FinishBlend, FGTipBinID, FGPackBinID, StartPack, FinishPack, 
+    IBCKG, BagSize, FullBags, PartBags, PackComplete, StartSieve2, FinishSieve2, FGItemkey, 
+    IBCFinished, Clean1start, Clean1finish, Clean2start, Clean2finish, Clean3start, Clean3finish
+FROM TFC_PTiming2
+WHERE ProcessCell = @line
+ORDER BY Sequence DESC";
+
+            DataTable dt = new DataTable();
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            using (SqlCommand cmd = new SqlCommand(sql, conn))
+            {
+                cmd.Parameters.AddWithValue("@line", selectedLine);
+                conn.Open();
+                dt.Load(cmd.ExecuteReader());
+            }
+
+            dataGridViewBatches.DataSource = dt;
+            CustomizeDataGridView();
+        }
+        private void CustomizeDataGridView()
+        {
+            var grid = dataGridViewBatches;
+
+            
+            grid.EnableHeadersVisualStyles = false;
+            grid.ColumnHeadersDefaultCellStyle.Font = new Font("Segoe UI", 16, FontStyle.Bold);
+            grid.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            grid.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(30, 30, 30); 
+            grid.ColumnHeadersDefaultCellStyle.ForeColor = Color.Gold; 
+            grid.ColumnHeadersHeight = 50; 
+
+            
+            grid.RowsDefaultCellStyle.BackColor = Color.White;
+            grid.AlternatingRowsDefaultCellStyle.BackColor = Color.FromArgb(220, 230, 240); 
+
+            
+            grid.DefaultCellStyle.Font = new Font("Segoe UI", 14);
+            grid.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            grid.DefaultCellStyle.Padding = new Padding(10, 5, 10, 5); 
+            grid.RowTemplate.Height = 60; 
+
+            
+            grid.CellBorderStyle = DataGridViewCellBorderStyle.Single;
+            grid.GridColor = Color.LightGray;
+
+            
+            grid.RowHeadersVisible = false;
+
+            
+            grid.DefaultCellStyle.SelectionBackColor = Color.FromArgb(255, 171, 64); 
+            grid.DefaultCellStyle.SelectionForeColor = Color.Black;
+
+            
+            foreach (DataGridViewColumn column in grid.Columns)
+            {
+                column.Width = 200; 
+            }
+
+            grid.ClearSelection();
+        }
+
+        private void DataGridViewBatches_CellFormatting(object sender, DataGridViewCellFormattingEventArgs e)
+        {
+            if (e.RowIndex < 0 || e.ColumnIndex < 0 || dataGridViewBatches.Rows[e.RowIndex].DataBoundItem == null)
+                return;
+
+            object batchNoValue = dataGridViewBatches.Rows[e.RowIndex].Cells["BatchNo"].Value;
+
+            
+            e.CellStyle.BackColor = e.RowIndex % 2 == 0 ? Color.White : Color.FromArgb(220, 230, 240);
+
+            
+            if (batchNoValue != null && batchNoValue.ToString() == "999999")
+            {
+                e.CellStyle.BackColor = Color.FromArgb(255, 200, 100); // Light Orange
+                e.CellStyle.ForeColor = Color.Black;
+                e.CellStyle.Font = new Font("Segoe UI", 14, FontStyle.Bold);
+            }
+        }
+
+    }
+}
